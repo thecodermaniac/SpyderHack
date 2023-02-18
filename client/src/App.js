d8913ee9d70afeb1fd8c93ad7420c9e95cb6c5b4
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-02-18 22:57:45 +0530

    all things added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Accordian from './components/Accordian';
+import Card from './components/Card';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <>
+      <Modal />
+      <Card />
+      <Accordian />
+    </>
+  );
+}
+
+export default App;
