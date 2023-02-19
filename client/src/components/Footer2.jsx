f5c5e89f420acf31d5d9c161ff387bc6daffd82c
Gopesh Goswami <ggopesh45@gmail.com>
2023-02-19 12:56:53 +0530

    Changed Mde

--- client/src/components/Footer2.jsx
+++ client/src/components/Footer2.jsx
@@ -6,21 +6,27 @@ import Facebook from "../images/socials/facebook.png";
 
 const Footer2 = () => {
   return (
-    <div className="">
+    <div>
       <img
         src={FooterBG}
         alt=""
         className=" -z-50 absolute w-screen left-0 h-1/2"
       />
-
-      <aside className=" text-white absolute">
-        <h1 className=" text-3xl font-bold">SDE Prep</h1>
-        <div className=" flex mt-10 gap-5">
-          <img src={Insta} alt="" className=" w-6" />
-          <img src={Linkedin} alt="" className=" w-6" />
-          <img src={Facebook} alt="" className=" w-6" />
-        </div>
-      </aside>
+      <div className=" flex justify-between ">
+        <aside className=" text-white absolute">
+          <h1 className=" text-3xl font-bold">SDE Prep</h1>
+          <div className=" flex mt-10 gap-5">
+            <img src={Insta} alt="" className=" w-6" />
+            <img src={Linkedin} alt="" className=" w-6" />
+            <img src={Facebook} alt="" className=" w-6" />
+          </div>
+          <h1 className=" mt-7">join our newsletter for daily updates!</h1>
+          <div className=" mt-5 flex gap-2">
+            <input type="text" className=" w-full" />
+            <button className=" px-3 py-1 bg-[#1E40AF]">Subscribe</button>
+          </div>
+        </aside>
+      </div>
     </div>
   );
 };
